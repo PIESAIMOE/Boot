302288e1fd427475e2306343dfdec4caea47fb1c
PIESAIMOE <ayekhantkhantsoe1@gmail.com>
2023-10-28 10:44:52 +0630

    style.css and index.html

---
+++ main.js
@@ -0,0 +1,16 @@
+// let reactBefore = document.getElementById("rateBefore");
+// let reactAfter = document.getElementById("rateAfter");
+
+// reactBefore.addEventListener("onclick",function(){
+//     console.log("click me");
+
+// })
+
+  // if(reactBefore.style.display = "block"){
+  //   reactBefore.style.display = "none";
+  //   reactAfter.style.display = "block";
+  // }else{
+  //   reactBefore.style.display ="block";
+  //   reactAfter.style.display = "none";
+
+  // }
