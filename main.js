ae21f6071a66ac8d3d4819224f3e4d5048a161b7
PIESAIMOE <ayekhantkhantsoe1@gmail.com>
2023-12-27 12:01:08 +0630

    fix

--- main.js
+++ main.js
@@ -1,16 +1,1 @@
-// let reactBefore = document.getElementById("rateBefore");
-// let reactAfter = document.getElementById("rateAfter");
 
-// reactBefore.addEventListener("onclick",function(){
-//     console.log("click me");
-
-// })
-
-  // if(reactBefore.style.display = "block"){
-  //   reactBefore.style.display = "none";
-  //   reactAfter.style.display = "block";
-  // }else{
-  //   reactBefore.style.display ="block";
-  //   reactAfter.style.display = "none";
-
-  // }
